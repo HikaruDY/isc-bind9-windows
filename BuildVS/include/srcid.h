2afbb2e7c30b3adc28777b8d77a97b83b5f27342
Hikaru Kurosaki <Hikaru@oss.xprj.org>
2023-04-12 23:08:15 +0900

    New 'BuildVS'

---
+++ BuildVS/include/srcid.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2012  Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * srcid.h * Generated automatically by Configure.pl.
+ * Date generated: Mon Apr 10 02:17:21 2023
+ */
+
+
+#ifndef  SRCID_H
+#define SRCID_H 1
+#define SRCID	"83e26c2"
+#endif /* SRCID_H */
---
+++ BuildVS/include/versions.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2001  Internet Systems Consortium, Inc. ("ISC")
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * versions.h.  Generated automatically by Configure.pl.
+ * Date generated: Mon Apr 10 02:17:21 2023
+ */
+
+
+#ifndef  VERSIONS_H
+#define VERSIONS_H 1
+
+#define VERSION "9.16.39"
+#define PRODUCT "BIND"
+
+#define DESCRIPTION "(Extended Support Version)"
+
+#define MAJOR "9.16"
+
+#define MAPAPI "3.0"
+
+#endif /* VERSIONS_H */
